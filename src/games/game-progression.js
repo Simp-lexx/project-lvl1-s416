9e2b5fddf66161bb02e3b480c00530f46d74c225
Simp-lexx <imlexx007@gmail.com>
2019-01-19 01:06:09 +1000

    game-progression created

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -1,44 +1,28 @@
-import { cons, car, cdr } from 'hexlet-pairs';
+import { cons } from 'hexlet-pairs';
 import { randInt } from '..';
 import gameEngine from '../game-engine';
 
-const minRandRange = 5;
-const maxRandRange = 35;
-const minOperRange = 0;
-const maxOperRange = 2;
-// const stepProgression = 2;
+const minProgrStart = 1;
+const maxProgrStart = 100;
+const minProgrStep = 0;
+const maxProgrStep = 10;
+const progressionLength = 10;
+const minProgrIndex = 0;
+const maxProgrIndex = 9;
+
 const description = 'What number is missing in the progression?\n';
 
-const operResult = (pair, operator) => {
-  const a = car(pair);
-  const b = cdr(pair);
-  let result = 0;
-  if (operator === 0) {
-    result = a + b;
-  } else if (operator === 1) {
-    result = a - b;
-  } else result = a * b;
-  return result;
-};
-
-const strOper = (operator) => {
-  let sign = '';
-  if (operator === 0) {
-    sign = '+';
-  } else if (operator === 1) {
-    sign = '-';
-  } else sign = '*';
-  return sign;
-};
-
 const brainProgression = () => {
-  const a = randInt(minRandRange, maxRandRange);
-  const b = randInt(minRandRange, maxRandRange);
-  const progrQuestion = cons(a, b);
-  const operator = randInt(minOperRange, maxOperRange);
-  const signOper = strOper(operator);
-  const answer = operResult(progrQuestion, operator);
-  const question = `${car(progrQuestion)} ${signOper} ${cdr(progrQuestion)}`;
+  const hiddenIndex = randInt(minProgrIndex, maxProgrIndex);
+  const progressionArray = [];
+  const startProgression = randInt(minProgrStart, maxProgrStart);
+  const stepProgression = randInt(minProgrStep, maxProgrStep);
+  for (let i = 0; i < progressionLength; i += 1) {
+    progressionArray[i] = startProgression + stepProgression * i;
+  }
+  const answer = progressionArray[hiddenIndex];
+  progressionArray[hiddenIndex] = '..';
+  const question = progressionArray.join(' ');
   return cons(question, answer);
 };
 
