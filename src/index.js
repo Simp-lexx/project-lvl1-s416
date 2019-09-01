b1fdd1b37c4fc9a8025adccbfedd3bac49a323b4
Simp-lexx <imlexx007@gmail.com>
2019-09-01 21:53:36 +1000

    update packages versions

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import { car, cdr } from 'hexlet-pairs';
 
 const roundQuantity = 3;
 
+const selectGame = () => {
+  console.log('Please enter number for select game to play: ');
+  console.log('1. Even game');
+  console.log('2. Calc game');
+  console.log('3. General common divisor game');
+  console.log('4. Progression game');
+  
+};
+
 const gameEngine = (description, generateGameData) => {
   console.log('Welcome to the Brain Games!');
   console.log(`${description}\n`);
